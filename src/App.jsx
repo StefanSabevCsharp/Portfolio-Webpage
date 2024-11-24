b7ff28e1e593c78859fbb2e99a63a8eebc717594
Stefan Sabev <stevenbg91@gmail.com>
2024-11-24 17:59:16 +0100

    Added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { Route, Router } from "react-router"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import { Routes } from "react-router"
+import About from "./components/about/About"
 
 function App() {
 
 
   return (
     <div className="container">
-     
-    <Header />
-    <Home />
+
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
     </div>
 
 
