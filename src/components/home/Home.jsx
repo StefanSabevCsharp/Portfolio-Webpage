99091b76b5a0485e7c4895cf5d91e0fbebbda598
Stefan Sabev <stevenbg91@gmail.com>
2024-11-24 17:59:29 +0100

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,19 +7,19 @@ export default function Home() {
             {/* personal image */}
             <img
                 className="rounded-full w-[250px] h-[250px] 2xl:w-[280px] 2xl:h-[280px]"
-                src="images/about/avatar.jpg"
+                src="images/mypic.jpg"
                 alt="about avatar"
             />
             <h3 className="mt-6 mb-1 text-5xl font-semibold dark:text-white">
                 {" "}
-                Monalisa Ashley{" "}
+                Stefan Sabev{" "}
             </h3>
-            <p className="mb-4 text-[#7B7B7B]">Ui/Ux Designer</p>
+            <p className="mb-4 text-[#7B7B7B]">FrondEnd Developer</p>
             {/* social link and social  buttons */}
             <div className="flex space-x-3">
-                {/* facebook link */}
+                
                 <a
-                    href="https://www.facebook.com/"
+                    href="https://www.facebook.com/stefan.sabev.94"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
@@ -27,21 +27,14 @@ export default function Home() {
                         <i className="fa-brands fa-facebook-f" />
                     </span>
                 </a>
-                {/* twitter link */}
-                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/StefanSabevCsharp" target="_blank" rel="noopener noreferrer">
                     <span className="socialbtn text-[#1C9CEA]">
-                        <i className="fa-brands fa-twitter" />
+                        <i className="fa-brands fa-github" />
                     </span>
                 </a>
-                {/* dribbble icon and link */}
-                <a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">
-                    <span className="socialbtn text-[#e14a84]">
-                        <i className="fa-brands fa-dribbble" />
-                    </span>
-                </a>
-                {/* linkedin icon and link */}
+               
                 <a
-                    href="https://www.linkedin.com/"
+                    href="https://www.linkedin.com/in/stefan-sabev-952a5620a/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
@@ -51,10 +44,12 @@ export default function Home() {
                 </a>
             </div>
             {/* dowanload button */}
-            <button className="dowanload-btn">
-                <img src="images/icons/dowanload.png" alt="icon" className="mr-2" />
-                Download CV{" "}
-            </button>
+            <a href="/public/CV.txt" download>
+                <button className="dowanload-btn">
+                    <img src="images/icons/dowanload.png" alt="icon" className="mr-2" />
+                    Download CV
+                </button>
+            </a>
         </div>
 
     );
