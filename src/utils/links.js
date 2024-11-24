bfde4ebbd37f9e3a1da57ef4b5e1ef3cecfaa68e
Stefan Sabev <stevenbg91@gmail.com>
2024-11-24 17:58:11 +0100

    Added abstract links

---
+++ src/utils/links.js
@@ -0,0 +1,10 @@
+const navLinks = [
+    {name: "Home", path: "/", image: "fa-solid fa-house"},
+    {name: "About", path: "/about", image: "fa-regular fa-user"},
+    {name: "Resume", path: "/resume", image: "fa-regular fa-file-lines"},
+    {name: "Works", path: "/portfolio", image: "fas fa-briefcase"},
+    {name: "Blogs", path: "/blog", image: "fa-brands fa-blogger"},
+    {name: "Contact", path: "/contact", image: "fa-solid fa-address-book"},
+]
+
+export default navLinks;
